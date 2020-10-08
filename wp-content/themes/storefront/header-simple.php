657062452e66ef2b907c6d16484c9312528e3054
Administrator <tregubov.a@tentorium.ru>
2020-10-08 14:52:25 +0500

    Initial commit

---
+++ wp-content/themes/storefront/header-simple.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package storefront
+ */
+
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/assets/icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/assets/icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/assets/icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/assets/icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/assets/icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/assets/icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/assets/icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/assets/icons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/assets/icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png">
+    <link rel="manifest" href="/assets/icons/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/assets/icons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php wp_body_open(); ?>
---
+++ wp-content/themes/storefront/page-start.php
@@ -0,0 +1,22 @@
+<?php
+get_header('simple');
+?>
+    <main>
+        <picture>
+            <img src="https://autoroom59.ru/wp-content/uploads/2020/10/blank.jpg" alt="">
+        </picture>
+
+        <div class="text__centered">
+            <h3>Контакты</h3>
+            <ul class="list__simple">
+                <li class="list_element list_element__simple"><a class="link__simple link__uppercase" href="tel:+79630156669">+7 963 015 66 69</a>
+                <li class="list_element list_element__simple"><a class="link__simple link__uppercase" href="tel:+79655684645">+7 965 568 46 45</a>
+                <li class="list_element list_element__simple"><a class="link__simple link__uppercase" href="https://go.2gis.com/pnfguu">г. Пермь, ул. Пихтовая, 37, корпус 2</a>
+                <li class="list_element list_element__simple"><a class="link__simple link__uppercase" href="https://vk.com/autoroomperm">vk.com/autoroomperm</a>
+                <li class="list_element list_element__simple"><a class="link__simple link__uppercase" href="mailto:autorum2016@yandex.ru">autorum2016@yandex.ru</a>
+            </ul>
+        </div>
+    </main>
+<?php
+
+get_footer('simple');
